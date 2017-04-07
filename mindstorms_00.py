1d319944e410d60b93d00f42c933dd18d561a606
adrioro <artistsvillage@me.com>
2017-04-07 09:09:40 -0400

    sixth commit

--- mindstorms_00.py
+++ mindstorms_00.py
@@ -1,3 +1,4 @@
+
 import turtle
 
 window = turtle.Screen()
@@ -8,33 +9,43 @@ brad.color("lightblue")
 brad.speed(9)
 brad.pensize(5)
 
-def draw_circle(): # Drawing a circle
+def draw_circle(): # Function to draw a circle
     angie = turtle.Turtle()
     angie.shape ("arrow")
     angie.color("yellow")
     angie.circle(100)
-    
     window.exitonclick()
 
-
-def action(): # Function to be used in my loop
+def action(): # Action to be used in my draw_square loop
     brad.forward(100)
     brad.right(90)
     return
+
+def action_t(): # Action to be used in my draw_triangle loop
+    brad.forward(100)
+    brad.right(120)
+    return
+
+def draw_triangle(): # Drawing a triangle
+    brad.pensize(11)
+    brad.color("white")
+    max_times = 3
+    times = 0
+    while times < 3:
+        action_t()
+        times = times +1
+    else:
+        draw_circle()
     
 
-def draw_square(): # Drawing a square and triangle   
+def draw_square(): # Drawing a square and circle   
     max_times = 4
-    times = 0
-    
+    times = 0    
     while True:
         while times < 4:
             action()
             times = times +1
         else:
-            while times < 3:
-                action()
-                times = times +1
-            draw_circle()
+            draw_triangle()       
 
 draw_square()
