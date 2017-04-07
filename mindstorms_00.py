91c34a3a5d73f93ed078271d0d3f74444323cd85
adrioro <artistsvillage@me.com>
2017-04-07 08:59:08 -0400

    second commit

--- mindstorms_00.py
+++ mindstorms_00.py
@@ -1,20 +1,43 @@
 import turtle
 
-def draw_square():
-    window = turtle.Screen()
-    window.bgcolor("red")
+window = turtle.Screen()
+window.bgcolor("red")
+brad = turtle.Turtle()
+brad.shape("turtle")
+brad.color("yellow")
+brad.speed(9)
 
-    brad = turtle.Turtle()
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
+def draw_circle():
+    angie = turtle.Turtle()
+    angie.shape ("arrow")
+    angie.color("blue")
+    angie.circle(100)
     
     window.exitonclick()
+
+
+def action(): # Function to be used in my loop
+    brad.forward(100)
+    brad.right(90)
+    return
+    
+
+def draw_square():    
+    max_times = 4
+    times = 0
+    
+    while True:
+        while times < 4:
+            action()
+            times = times +1
+        else:
+            draw_circle()
+
+
+        
+
+        
+
 
 
 
